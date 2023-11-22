055810248cd5977332b015963625f15ced9c8c97
anubhav1772 <anubhavsingh55182@gmail.com>
2023-11-22 20:13:28 +0530

    cmake build dir

---
+++ build/CMakeFiles/rank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rank.dir/LinearAlgebra/MatrixRank.cpp.o"
+  "rank"
+  "rank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
